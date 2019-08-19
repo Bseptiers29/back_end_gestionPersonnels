26b2f0ce8302964acea4c847040cecb7b1509cf2
Sacris <bastienseptiers.emploi@gmail.com>
2019-08-19 14:23:43 +0200

    [Erreurs]Tentative de résolution du problème de PARSE

--- server/server.js
+++ server/server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const routes = require("./routes");
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/v1", routes);
 
