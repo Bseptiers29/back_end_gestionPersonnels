bae36b13b0ad5ae21760b395799a594d4340689d
Sacris <bastienseptiers.emploi@gmail.com>
2019-08-21 17:23:56 +0200

    [Essais]Premiers essais de récupération d'image

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+var multer = require("multer");
 
 const routes = require("./routes");
 
@@ -13,12 +14,51 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/v1", routes);
 
+app.listen(process.env.PORT || "3000", () => {
+  console.log(`Server is running on port: ${process.env.PORT || "3000"}`);
+});
+
 app.get("/", function(req, res) {
   res.send(
     "Bienvenu sur l'API de l'outil de gestion de personnels SOCOMORE, vous trouverez vos ressources a ces url :<br/><br/> - Personnels : /v1/personnels <br/><br/> - Conges : /v1/conges "
   );
 });
 
-app.listen(process.env.PORT || "3000", () => {
-  console.log(`Server is running on port: ${process.env.PORT || "3000"}`);
+// Essais
+const handleError = (err, res) => {
+  res
+    .status(500)
+    .contentType("text/plain")
+    .end("Oops! Something went wrong!");
+};
+
+const upload = multer({
+  dest: "../tmp"
+});
+
+app.get("/try", express.static(path.join(__dirname, "./public")));
+
+app.post("/profile", upload.single("photoPersonnel"), function(req, res) {
+  const tempPath = req.file.path;
+  const targetPath = path.join(__dirname, "../static/image.png");
+
+  if (path.extname(req.file.originalname).toLowerCase() === ".png") {
+    fs.rename(tempPath, targetPath, err => {
+      if (err) return handleError(err, res);
+
+      res
+        .status(200)
+        .contentType("text/plain")
+        .end("File uploaded!");
+    });
+  } else {
+    fs.unlink(tempPath, err => {
+      if (err) return handleError(err, res);
+
+      res
+        .status(403)
+        .contentType("text/plain")
+        .end("Only .png files are allowed!");
+    });
+  }
 });
